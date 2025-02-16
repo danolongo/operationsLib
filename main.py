f2b73392150b69f8ce250132393a9722ab6cefe9
danolongo <danielmarg04@gmail.com>
2025-02-16 02:54:45 -0600

    new changes

--- main.py
+++ main.py
@@ -1,43 +1,65 @@
-import numpy as np
-''''
-for Matrix.columns in Matrix.data:
-    for Matrix.rows in Matrix.data:
-'''
-
-exampleData =   np.array([[1, 1, 4, 0],
-                        [1, 2, -3, -2],
-                        [3, 8, -23, -9]])
-
-
-# elementary operations
+# elementary operations!
 # op1: multiply any equation by non-zero number on both sides
-def op1(data, num, row):
-    data = np.array(data)
-    for i in range(0, len(data[0])):
-        data[i:i][row:row] = num * data[i:i][row:row]
+def rowMult(arr, num, row): 
+    # take row to be multiplied
+    multBy = arr[i][row]
 
-print(exampleData)
-op1(exampleData, 10, 1)
-print(exampleData)
+    # multiply row by num
+    for i in len(arr[i]):
+        arr = num * arr[i][row]
+
+    # return
+    return arr
 
 # op2: multiply an equation by a scalar and add to another equation
-# def op2(data):
+def rowAdd(arr1, num, row1, row2):
+    # num*row1 + row2 -> row2
+    # use rowMult
+
+    for i in arr1[i]:
+        arr1 = rowMult(arr1[i], num, row1) + row2[i]
+    
+    return arr1
 
 def rowEchelon():
+    # make 1st pivot 1
+    
+
+    # make everything under = zero
+
+    # do that over and over
 
 def augmentedRowEchelon():
 
+
 def inverse():
+
 
 def transpose():
 
-def addMatrix():
 
-def subtractMatrix():
+def addMatrix(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return None
+    
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+    return result
+
+def subtractMatrix(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return None
+    
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] - arr2[i])
+    return result
 
 def matrixMult():
 
 def dotProd():
+    
 
 def getDeterminant():
 
